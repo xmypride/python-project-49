6ebe58f169afdd467bbba1325f04a56d13486e3f
xmypride <x.my-pride.x@yandex.ru>
2023-02-12 16:38:51 +0300

    third_step

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+from brain_games import cli
+
+
 def greet():
     print('Welcome to the Brain Games!')
 
 def main():
     greet()
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
+
